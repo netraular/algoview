a6ba843a07e6dafc2672c25d54fab271490ebf25
netraular <netraular@gmail.com>
2024-07-08 11:22:37 +0000

    rutas arregladas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,3 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
