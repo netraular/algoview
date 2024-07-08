1e7ab64a7b052886ee06a1e66e4e39e8988d69a9
netraular <netraular@gmail.com>
2024-07-08 11:05:28 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
