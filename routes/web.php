be72ca36c5e3d942e717300a6b6102bc9d59551c
netraular <netraular@gmail.com>
2024-07-08 11:34:54 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
