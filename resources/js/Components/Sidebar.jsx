7b7feb37ef1b175ab868b08e49c240f1f51541d7
netraular <netraular@gmail.com>
2024-07-28 16:56:38 +0000

    Made sidebar with react-bootstrap

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from '@inertiajs/react';
 import ThemeToggle from './ThemeToggle';
 import JiraButton from './Dev/JiraButton';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import '../../css/Sidebar.css';
 
 const Sidebar = ({ algorithms, onSelectAlgorithm, isOpen, isMobile, navbarHeight }) => {
     const sidebarStyle = {
@@ -14,24 +18,57 @@ const Sidebar = ({ algorithms, onSelectAlgorithm, isOpen, isMobile, navbarHeight
     };
 
     return (
-        <div className="d-flex flex-column bg-light p-3" style={sidebarStyle}>
+        <div className="d-flex flex-column bg-light p-3" style={sidebarStyle} id='MainMenu'>
             {!isMobile && (
                 <Link href={route('welcome')} className="d-flex justify-content-between align-items-center text-decoration-none">
-                    <img src="/favicon.png" alt="Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />       
+                    <img src="/favicon.png" alt="Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
                     <h4 className="mb-0">
                         <b style={{ fontFamily: 'PixelifySans', color: 'red', marginRight: '10px' }}> Algorithm</b>
-                        <b style={{ fontFamily: 'PixelifySans', color: 'Coral' }}>Xplore</b>             
+                        <b style={{ fontFamily: 'PixelifySans', color: 'Coral' }}>Xplore</b>
                     </h4>
                 </Link>
             )}
-            <ul className="list-group flex-grow-1 mt-3">
-                {algorithms.map((algorithm, index) => (
-                    <li key={index} className="list-group-item" onClick={() => onSelectAlgorithm(algorithm)}>
-                        {algorithm.name}
-                    </li>
-                ))}
-            </ul>
-            <JiraButton/>
+            <Accordion defaultActiveKey="0">
+                <Card className='custom-card'>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header className='custom-card-header'>Sorting Algorithms</Accordion.Header>
+                        <Accordion.Body className='custom-card-body'>
+                            <ListGroup variant="flush">
+                            {algorithms.map((algorithm, index) => (
+                                <ListGroup.Item
+                                key={index}
+                                action
+                                className='custom-card-item'
+                                onClick={() => onSelectAlgorithm(algorithm)}
+                                >
+                                {algorithm.name}
+                                </ListGroup.Item>
+                            ))}
+                            </ListGroup>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Card>
+                <Card className='custom-card'>
+                    <Accordion.Item eventKey="1">
+                    <Accordion.Header className='custom-card-header'>Finding Algorithms</Accordion.Header>
+                    <Accordion.Body className='custom-card-body'>
+                        <ListGroup variant="flush">
+                        {algorithms.map((algorithm, index) => (
+                            <ListGroup.Item
+                            key={index}
+                            action
+                            className='custom-card-item'
+                            onClick={() => onSelectAlgorithm(algorithm)}
+                            >
+                            {algorithm.name}
+                            </ListGroup.Item>
+                        ))}
+                        </ListGroup>
+                    </Accordion.Body>
+                    </Accordion.Item>
+                </Card>
+            </Accordion>
+            <JiraButton />
             <div className="mt-3 d-flex mb-3">
                 <a title="GitHub Repository" href="https://github.com/netraular/algoview" target="_blank" rel="noopener noreferrer" className="btn btn-secondary me-2">
                     <i className="bi bi-github"></i> Github
@@ -43,7 +80,7 @@ const Sidebar = ({ algorithms, onSelectAlgorithm, isOpen, isMobile, navbarHeight
             <div className="d-flex justify-content-between align-items-center mt-auto">
                 <span>© Netshiba</span>
                 <ThemeToggle />
-            </div>
+            </div>      
         </div>
     );
 };
